7ba8f98e4ad803070147038ead249d67843e67fd
Bryson Monteiro <brysonmonteiro@brysons-mbp.mynetworksettings.com>
2024-03-11 19:03:23 -0400

    finished NavBar-hamburger section

--- portfolio/src/components/navBar/components.tsx
+++ portfolio/src/components/navBar/components.tsx
@@ -6,8 +6,21 @@ import { motion } from "framer-motion";
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const overlayVariants = {
+    initial: {
+      scale: 1, // Start at normal size
+      opacity: 0,
+    },
+    hover: {
+      scale: 1.05, // Scale up a bit on hover
+      opacity: 1,
+      transition: { duration: 0.2, ease: "easeInOut" }, // Quick transition for pop effect
+    },
+    // Return to normal size after hover or add additional states as needed
+  };
+
   const lineVariants = {
-    opened: { rotate: 480, y: 10 },
+    opened: { rotate: 490, y: 10 },
     closed: { rotate: 0, y: 0 },
   };
   const midLineVariants = {
@@ -15,12 +28,12 @@ const NavBar = () => {
     closed: { opacity: 1 },
   };
   const bottomLineVariants = {
-    opened: { rotate: -480, y: -10 },
+    opened: { rotate: -490, y: -10 },
     closed: { rotate: 0, y: 0 },
   };
 
   return (
-    <nav className="flex items-center p-4 bg-galaxy-gradient text-galaxy-white w-full">
+    <nav className="flex items-center p-4 bg-galaxy-gradient text-white w-full">
       {/* Flex container for centering items on all screens, justify-between on desktop */}
       <div className="flex items-center justify-center md:justify-between w-full max-w-4xl mx-auto">
         {/* Left Links (visible on desktop only) */}
@@ -58,7 +71,7 @@ const NavBar = () => {
           className="md:hidden z-50 ml-auto"
           onClick={() => setIsOpen(!isOpen)}
         >
-          <motion.div className="w-8 h-8 flex flex-col justify-center items-center space-y-2">
+          <motion.div className="w-8 h-8  flex flex-col justify-center items-center space-y-2">
             <div className="md:hidden z-50" onClick={() => setIsOpen(!isOpen)}>
               <motion.div className="flex flex-col space-y-2">
                 <motion.div
@@ -83,12 +96,105 @@ const NavBar = () => {
       </div>
       {isOpen && (
         <motion.div
-          initial={{ x: "100%" }}
-          animate={{ x: 0 }}
-          exit={{ x: "100%" }}
+          initial={{ y: "-100%" }} // Start above the view
+          animate={{ y: 0 }} // Move to original position
+          exit={{ y: "-100%" }} // Exit to the top
           transition={{ type: "spring", stiffness: 30, duration: 0.5 }}
-          className="absolute top-0 right-0 h-full w-[75%] bg-black text-white p-8 flex flex-col space-y-4 md:hidden"
-        ></motion.div>
+          className="absolute top-0 right-0 h-full w-full bg-black bg-opacity-65 text-white flex flex-col space-y-3 md:hidden"
+        >
+          <motion.div
+            className="mt-23 h-12 w-full rounded overflow-hidden relative bg-galaxy-gradient"
+            initial="initial"
+            whileHover="hover"
+          >
+            {/* Overlay with the second gradient */}
+            <motion.div
+              className="absolute inset-0 bg-galaxy-inv"
+              variants={overlayVariants}
+              transition={{ duration: 0.5, ease: "easeInOut" }}
+            />
+            <motion.button
+              className="absolute inset-0 w-full h-full font-bold text-xl"
+              variants={{}}
+              whileHover={{
+                scale: 1.1,
+                transition: { duration: 0.2 },
+              }}
+              whileTap={{ scale: 0.9 }}
+            >
+              About
+            </motion.button>
+          </motion.div>
+          <motion.div
+            className="mt-23 h-12 w-full rounded overflow-hidden relative bg-galaxy-gradient"
+            initial="initial"
+            whileHover="hover"
+          >
+            {/* Overlay with the second gradient */}
+            <motion.div
+              className="absolute inset-0 bg-galaxy-inv"
+              variants={overlayVariants}
+              transition={{ duration: 0.5, ease: "easeInOut" }}
+            />
+            <motion.button
+              className="absolute inset-0 w-full h-full font-bold text-xl"
+              variants={{}}
+              whileHover={{
+                scale: 1.1,
+                transition: { duration: 0.2 },
+              }}
+              whileTap={{ scale: 0.9 }}
+            >
+              Resume
+            </motion.button>
+          </motion.div>
+          <motion.div
+            className="mt-23 h-12 w-full rounded overflow-hidden relative bg-galaxy-gradient"
+            initial="initial"
+            whileHover="hover"
+          >
+            {/* Overlay with the second gradient */}
+            <motion.div
+              className="absolute inset-0 bg-galaxy-inv"
+              variants={overlayVariants}
+              transition={{ duration: 0.5, ease: "easeInOut" }}
+            />
+            <motion.button
+              className="absolute inset-0 w-full h-full font-bold text-xl"
+              variants={{}}
+              whileHover={{
+                scale: 1.1,
+                transition: { duration: 0.2 },
+              }}
+              whileTap={{ scale: 0.9 }}
+            >
+              Skills
+            </motion.button>
+          </motion.div>
+          <motion.div
+            className="mt-23 h-12 w-full rounded overflow-hidden relative bg-galaxy-gradient"
+            initial="initial"
+            whileHover="hover"
+          >
+            {/* Overlay with the second gradient */}
+            <motion.div
+              className="absolute inset-0 bg-galaxy-inv"
+              variants={overlayVariants}
+              transition={{ duration: 0.5, ease: "easeInOut" }}
+            />
+            <motion.button
+              className="absolute inset-0 w-full h-full font-bold text-xl"
+              variants={{}}
+              whileHover={{
+                scale: 1.1,
+                transition: { duration: 0.2 },
+              }}
+              whileTap={{ scale: 0.9 }}
+            >
+              Contact Me
+            </motion.button>
+          </motion.div>
+        </motion.div>
       )}
     </nav>
   );
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -10,16 +10,20 @@ export default {
           lightpurple: "#2a3b8f",
           deepurple: "#3E0B51",
           brightpurple: "#7D5BA6",
-          white: "#ffffff",
-          black: "#000000",
         },
       },
       backgroundImage: {
         "galaxy-gradient":
           "linear-gradient(to right, #1d1e5f, #3E0B51, #0a0f38, #7D5BA6)",
+        "galaxy-inv": "linear-gradient(to right, #0f0c29, #302b63, #24243e)",
       },
       fontFamily: {
         sans: ["Helvetica", "Arial", "sans-serif"],
+      },
+      spacing: {
+        21: "6rem",
+        22: "7rem",
+        23: "8rem",
       },
     },
   },
